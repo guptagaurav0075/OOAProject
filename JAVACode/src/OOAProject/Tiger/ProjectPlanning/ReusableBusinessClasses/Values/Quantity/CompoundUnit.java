40a2779dd7cb1dab13fe95dce088048e9548464a
GreatStoneDragon <abdullah.alshehabi@gmail.com>
2016-12-01 17:26:25 -0800

    Added Quantity

---
+++ JAVACode/src/OOAProject/Tiger/ProjectPlanning/ReusableBusinessClasses/Values/Quantity/CompoundUnit.java
@@ -0,0 +1,28 @@
+package OOAProject.Tiger.ProjectPlanning.ReusableBusinessClasses.Values.Quantity;
+
+import Values.Quantity.Unit;
+import Values.Quantity.UnitComposer;
+
+/**
+ * Created by Abdullah Al-Shehabi on 11/2/2016.
+ */
+class CompoundUnit extends Unit {
+    private Unit unit1;
+    private Unit unit2;
+    private UnitComposer composer;
+    public CompoundUnit(Unit unit1, UnitComposer composer, Unit unit2) {
+        super(unit1.toString() + " " + composer + " " + unit2.toString());
+        this.unit1 = unit1;
+        this.unit2 = unit2;
+        this.composer = composer;
+    }
+    public Unit getUnit1() {
+        return unit1;
+    }
+    public Unit getUnit2() {
+        return unit2;
+    }
+    public UnitComposer getComposer() {
+        return composer;
+    }
+}
